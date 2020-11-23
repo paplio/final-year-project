8b43a088112da739a47998a16a7131c4ac05a1d6
paplio <ani.12.28.98@gmail.com>
2020-11-23 12:12:02 +0530

    Added vbasic REST-API integration to check if it works on localhost from computer.

--- server/app.py
+++ server/app.py
@@ -1,1 +1,22 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+@app.route("/")
+def default():
+	return "Testing client-server connectivity on localhost!"
+
+@app.route("/upload")
+def upload():
+    return "This is the API endpoint we are trying to hit."
+
+if __name__ == '__main__':
+    app.run(port=8000)
