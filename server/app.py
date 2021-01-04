e89342e2761dc39713c316d4f670ac0ba9845a67
paplio <ani.12.28.98@gmail.com>
2021-01-04 12:03:41 +0530

    Add files via upload

--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask import send_file
 from flask_restful import Resource, Api
+import subtitle_ing
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,5 +21,29 @@ def default():
 def upload():
     return "This is the API endpoint we are trying to hit."
 
+@app.route("/manualupload")
+def manualupload():
+	return render_template('manual.html')
+
+@app.route("/converting", methods = ['GET', 'POST'])
+def converting():
+	if request.method == 'POST':
+		f = request.files['myfile']
+		f.save(os.path.join("static/",f.filename))
+		subtitle_ing.convert(f.filename)
+		pathThing = os.path.join("" + "subbed-"+f.filename)
+		return download(pathThing)
+
+@app.route("/viewfile")
+def showfile(filename):
+	return render_template('showfile.html', filename = filename)
+
+@app.route("/download")
+def download(pathString):
+	try:
+		return send_file(pathString, attachment_filename='subbed-video.mp4', as_attachment=True)
+	except Exception as e:
+		print 'huh'
+
 if __name__ == '__main__':
-    app.run(port=8000)
+	app.run( port=8000)
